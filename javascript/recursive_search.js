65dd4aa6b54139317404aa0950b94558ae1a0a68
Dorahely Sanchez <dorahelys@gmail.com>
2024-04-26 10:24:59 -0700

    explanation

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -2,6 +2,8 @@ function recursiveSearch(arr, target) {
   // type your code here
 }
 
+//non recursive way - with loops
+
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: true");
@@ -15,5 +17,29 @@ if (require.main === module) {
 
 module.exports = recursiveSearch;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+//writing down basic code explanations
+//difference between for...in  , for...of , and traditional for loop ( i = 0; i < arr.length; i++)
+
+// for...in
+//iterating over object properties 
+//in very basic wording: 
+//let's say I have a box of numbers, letter, and shapes and I also have a corresponding tag that goes with them
+//with this loop it is like looking at each tag but the issue arries when there are more tags than things in the box
+//or I just want to look at letters but may accidentally look at numbers
+//more time to waste because I have to read all the tags first can't just focus on one thing
+//for example for this method I have to look at the numbers tags and shapes tags one by one even though I only want the letters
+
+//for...of
+//lets say I just want shapes again
+//this one is like taking out each shape from the box this is great for arrays
+//because it allows for just that shapes to be the focus not numbers or letters
+//don't need to look at each element one by one
+
+//traditional for loop
+//this like counting each element in the box as I take them out one by one
+//good if I want to do anything additional with the items in teh box or if I want to sure I looked at each one.
+
+
+
+
+
