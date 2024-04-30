d121842f9be139d948ca57436d2e6834b16b7427
Dorahely Sanchez <dorahelys@gmail.com>
2024-04-30 17:27:17 -0500

    completed

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,17 +1,33 @@
 //input: [2,3,7,9], 3
 //output: true 
-function recursiveSearch(arr, target) {
-  //starting at first element in arr check if element in arr is the target 
-  for (const value of arr) {
-    if (value === target) {
-      //return true once found
-      return true
-    }
-    //call function to then repeat the process
-    else if (value != target) {
-      recursiveSearch()
-    }
+
+//starting with three parameters the array, the target to search for, and current index which is starting at 0
+function recursiveSearch(arr, target, index = 0) {
+  //base case: if current element equals target, return true
+  //arr[index] is the value stored at that index!
+  if (arr[index] === target) {
+    return true;
   }
+  //If the index equals the last element in my array , return false
+  //this line will search entire array and it target not found then returns false
+  //when to use arr.length or arr.length - 1?
+  //arr.length is good for knowing how many elements in array: for ex if I have 3 then length would be 3
+  //arr.length - 1 is good for knowing the last index of last element in my array so if I have 3  then idex would be 2
+  //when I need to specifically reference the last element of my array use arr.length - 1 
+  //^or want to make sure my loop terminates at the end of the array & want it to stop at last element
+  if (index === arr.length - 1) {
+    return false;
+  }
+  
+  //recursive case:
+  //calling the function itself(aka recursion)
+  //then takes 3 arguments
+  //first is the array that is being search which ensures that the recursive call continues to only search within the same array
+  //second is the target aka value that is looking for this is done again to make sure it for teh recurive call that all call are searching for the same target/value
+  //index + 1 is the increment value - makes sure that after each recurive call it moves on to the next element or value in the array 
+  //which allow it to then search the next value without this step the function would just get stuck on that value or index
+
+  return recursiveSearch(arr, target, index + 1);
 }
 
 /*
@@ -47,15 +63,31 @@ function traditionalForLoop(arr, target){
 }
 */
 
+//traditional loop ex again:
+/*
+function recursiveSearch(arr, target) {
+  // Iterate through the array using a for loop
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i];
+    // If current element equals target, return true
+    if (value === target) {
+      return true;
+    }
+  }
+  // If the loop completes without finding the target, return false
+  return false;
+}
+*/
+
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: true");
-  console.log("=>", recursiveSearch([1, 2, 3], 2));
+  console.log("=>", recursiveSearch([1, 2, 3, 4, 7], 8));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", recursiveSearch([3, 2, 1], 4));
+  console.log("=>", recursiveSearch([3, 2, 1, 8], 1));
 }
 
 module.exports = recursiveSearch;
